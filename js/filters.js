ac5ff4818b98744dc909de39e7d09ae3f38c5b65
iu-shvetsova <iu-shvetsova69@yandex.ru>
2019-07-22 20:37:54 +0500

    Добавлена фильтрация по всем категориям

--- js/filters.js
+++ js/filters.js
@@ -4,16 +4,97 @@
   var initialAds = [];
 
   var filterByType = document.querySelector('#housing-type');
+  var filterByPrice = document.querySelector('#housing-price');
+  var filterByRooms = document.querySelector('#housing-rooms');
+  var filterByGuests = document.querySelector('#housing-guests');
+  var filterByFeatures = document.querySelector('#housing-features');
 
-  var onTypeChange = function () {
-    var filteredAds;
+  var features = filterByFeatures.querySelectorAll('.map__checkbox');
+
+  var getAdsByType = function (ads) {
     if (filterByType.value === 'any') {
-      filteredAds = initialAds;
-    } else {
-      filteredAds = initialAds.filter(function (ad) {
-        return ad.offer.type === filterByType.value;
-      });
+      return ads;
     }
+    return ads.filter(function (ad) {
+      return ad.offer.type === filterByType.value;
+    });
+  };
+
+  var getAdsByRooms = function (ads) {
+    if (filterByRooms.value === 'any') {
+      return ads;
+    }
+    return ads.filter(function (ad) {
+      return ad.offer.rooms.toString() === filterByRooms.value;
+    });
+  };
+
+  var getAdsByGuests = function (ads) {
+    if (filterByGuests.value === 'any') {
+      return ads;
+    }
+    return ads.filter(function (ad) {
+      return ad.offer.guests.toString() === filterByGuests.value;
+    });
+  };
+
+  var getAdsByPrice = function (ads) {
+    switch (filterByPrice.value) {
+      case 'any':
+        return ads;
+      case 'low':
+        return ads.filter(function (ad) {
+          return ad.offer.price <= 10000;
+        });
+      case 'middle':
+        return ads.filter(function (ad) {
+          return (ad.offer.price > 10000) && (ad.offer.price <= 50000);
+        });
+      case 'high':
+        return ads.filter(function (ad) {
+          return ad.offer.price > 50000;
+        });
+    }
+    return null;
+  };
+
+  var getAdsByFeatures = function (ads) {
+    var checkedFeatures = [];
+    var filteredAds = [];
+
+    features.forEach(function (feature) {
+      if (feature.checked) {
+        checkedFeatures.push(feature);
+      }
+    });
+
+    if (checkedFeatures.length === 0) {
+      return ads;
+    }
+
+    ads.forEach(function (ad) {
+      checkedFeatures.forEach(function (checkedFeature) {
+        if (ad.offer.features.some(function (feature) {
+          return feature === checkedFeature.value;
+        })) {
+          filteredAds.push(ad);
+        }
+      });
+    });
+
+    return filteredAds;
+
+  };
+
+  var onChange = function () {
+    var filteredAds = initialAds;
+
+    filteredAds = getAdsByType(filteredAds);
+    filteredAds = getAdsByRooms(filteredAds);
+    filteredAds = getAdsByGuests(filteredAds);
+    filteredAds = getAdsByPrice(filteredAds);
+    filteredAds = getAdsByFeatures(filteredAds);
+
     window.pins.draw(filteredAds);
   };
 
@@ -22,7 +103,13 @@
       initialAds = ads;
       window.pins.draw(initialAds);
 
-      filterByType.addEventListener('change', onTypeChange);
+      filterByType.addEventListener('change', onChange);
+      filterByRooms.addEventListener('change', onChange);
+      filterByGuests.addEventListener('change', onChange);
+      filterByPrice.addEventListener('change', onChange);
+      features.forEach(function (feature) {
+        feature.addEventListener('click', onChange);
+      });
     }
   };
 })();
